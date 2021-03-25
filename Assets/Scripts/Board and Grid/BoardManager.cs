2b449562d4bd0c98e5093c89bbe51dcdd13d2045
sh960440 <54958110+sh960440@users.noreply.github.com>
2021-03-25 04:38:09 -0400

    Layout

---
+++ Assets/Scripts/Board and Grid/BoardManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BoardManager : MonoBehaviour 
+{
+	public static BoardManager instance; // Singleton
+	public List<Sprite> characters = new List<Sprite>(); // A list of sprites used as tile pieces.
+	public GameObject tile; // The prefab instantiated when creating the board
+	public int xSize, ySize; // X and Y dimensions of the board.
+
+	private GameObject[,] tiles; // Used to store the tiles in the board
+
+	public bool IsShifting { get; set; } // Tell the game when a match is found and the board is re-filling
+
+	void Start () 
+	{
+		instance = GetComponent<BoardManager>(); // Sets the singleton with reference of the BoardManager
+
+		Vector2 offset = tile.GetComponent<SpriteRenderer>().bounds.size;
+        CreateBoard(offset.x, offset.y);
+    }
+
+	private void CreateBoard (float xOffset, float yOffset) {
+		tiles = new GameObject[xSize, ySize]; // The 2D array tiles gets initialized
+
+		// Find the starting positions for the board generation
+        float startX = transform.position.x;
+		float startY = transform.position.y;
+
+		Sprite[] previousLeft = new Sprite[ySize];
+    	Sprite previousBelow = null;
+
+
+		// Loop through xSize and ySize, instantiating a newTile every iteration to achieve a grid of rows and columns
+		for (int x = 0; x < xSize; x++) 
+		{
+			for (int y = 0; y < ySize; y++) 
+			{
+				GameObject newTile = Instantiate(tile, new Vector3(startX + (xOffset * x), startY + (yOffset * y), 0), tile.transform.rotation);
+				tiles[x, y] = newTile;
+
+				newTile.transform.parent = transform; // Parent all the tiles to BoardManager
+				
+				List<Sprite> possibleCharacters = new List<Sprite>(); // Create a list of possible characters for this sprite
+				possibleCharacters.AddRange(characters); // Add all characters to the list
+				
+				// Remove the characters that are on the left and below the current sprite from the list
+				possibleCharacters.Remove(previousLeft[y]);
+				possibleCharacters.Remove(previousBelow);
+				Sprite newSprite = possibleCharacters[Random.Range(0, possibleCharacters.Count)]; // Randomly choose a sprite
+				newTile.GetComponent<SpriteRenderer>().sprite = newSprite; // Set the newly created tile's sprite to the randomly chosen sprite
+
+				previousLeft[y] = newSprite;
+				previousBelow = newSprite;
+			}
+        }
+    }
+}
---
+++ Assets/Scripts/Board and Grid/Tile.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Tile : MonoBehaviour 
+{
+	private static Color selectedColor = new Color(.5f, .5f, .5f, 1.0f);
+	private static Tile previousSelected = null;
+
+	private SpriteRenderer render;
+	private bool isSelected = false;
+
+	private Vector2[] adjacentDirections = new Vector2[] { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
+
+	void Awake() 
+	{
+		render = GetComponent<SpriteRenderer>();
+    }
+
+	private void Select() 
+	{
+		isSelected = true;
+		render.color = selectedColor;
+		previousSelected = gameObject.GetComponent<Tile>();
+		SFXManager.instance.PlaySFX(Clip.Select);
+	}
+
+	private void Deselect() 
+	{
+		isSelected = false;
+		render.color = Color.white;
+		previousSelected = null;
+	}
+
+	void OnMouseDown() 
+	{
+		// Make sure the game is permitting tile selections
+		if (render.sprite == null || BoardManager.instance.IsShifting) return;
+
+		if (isSelected) // Determines whether to select or deselect the tile
+		{
+			Deselect();
+		} 
+		else 
+		{
+			if (previousSelected == null) // Check if there's already another tile selected
+			{
+				Select();
+				Debug.Log("First");
+			} 
+			else 
+			{
+				if (GetAllAdjacentTiles().Contains(previousSelected.gameObject)) // Check if the previousSelected game object is in the returned adjacent tiles list
+				{
+					Debug.Log("Is an adjacent tile");
+					SwapSprite(previousSelected.render); // Swap the sprite of the tile
+					previousSelected.Deselect(); // If it wasn't the first one that was selected, deselect all tiles
+				} 
+				else // The tile isn't next to the previously selected one
+				{
+					Debug.Log("Is NOT an adjacent tile");
+					previousSelected.GetComponent<Tile>().Deselect(); // Deselect the previous one
+					Select(); // Select the newly selected tile
+				}
+			}
+    	}
+	}
+
+	public void SwapSprite(SpriteRenderer render2) 
+	{
+		if (render.sprite == render2.sprite) return; // Check render2 against the SpriteRenderer of the current tile
+
+		Sprite tempSprite = render2.sprite; // Create a tempSprite to hold the sprite of render2
+		render2.sprite = render.sprite; // Swap out the second sprite by setting it to the first
+		render.sprite = tempSprite; // Swap out the first sprite by setting it to the second
+		SFXManager.instance.PlaySFX(Clip.Swap); // Play a sound effect
+	}
+
+	private GameObject GetAdjacent(Vector2 castDir) // Retrieve a single adjacent tile by sending a raycast in the target specified by castDir
+	{
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, castDir);
+		if (hit.collider != null) 
+		{
+			return hit.collider.gameObject;
+		}
+		return null;
+	}
+
+	private List<GameObject> GetAllAdjacentTiles() // Uses GetAdjacent() to generate a list of tiles surrounding the current tile
+	{
+		List<GameObject> adjacentTiles = new List<GameObject>();
+		for (int i = 0; i < adjacentDirections.Length; i++) 
+		{
+			adjacentTiles.Add(GetAdjacent(adjacentDirections[i]));
+		}
+		return adjacentTiles;
+	}
+}
